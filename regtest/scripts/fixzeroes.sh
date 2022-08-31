eb5cf42629e8719f8433392a3723fc46cfb9609e
Giovanni Bussi <giovanni.bussi@gmail.com>
2022-08-31 17:36:35 +0200

    use dos2unix on windows as well

--- regtest/scripts/fixzeroes.sh
+++ regtest/scripts/fixzeroes.sh
@@ -4,9 +4,9 @@ file=$1
 
 grep -q "\x00" $file  && exit 0
 
+test -n "$PLUMED_REGTEST_DOS2UNIX" && $PLUMED_REGTEST_DOS2UNIX $file
+
 sed "s/-\(0\.0*0 \)/ \1/g; 
      s/-\(0\.0*0$\)/ \1/g" $file > $file.$$.tmp
 mv $file.$$.tmp $file
-
-test -n "$PLUMED_REGTEST_DOS2UNIX" && $PLUMED_REGTEST_DOS2UNIX $file
 
